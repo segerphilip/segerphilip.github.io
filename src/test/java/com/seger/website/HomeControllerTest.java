18115bb7ce6bf914ccfed58a2860823b9e9d56e9
Philip Seger <seger.philip@gmail.com>
2022-02-13 11:54:20 -0500

    Spring redesign of website

---
+++ src/test/java/com/seger/website/HomeControllerTest.java
@@ -0,0 +1,13 @@
+package com.seger.website;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+    @Test
+    void mainPage() {
+
+    }
+}
